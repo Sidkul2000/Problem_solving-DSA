614499bc962acc9282e888c66dbfe75709f10269
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2025-01-17 13:17:17 -0500

    Time: 0 ms (100%), Space: 18 MB (10.11%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.py
@@ -1,24 +1,22 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        n = len(arr)
-        s = 1
-        arr.sort()
-        print(arr)
-        ans = {}
-        if n == 1:
-            return True
-        for i in range(n-1):
-            if arr[i+1] == arr[i]:
-                s += 1
-            else:
-                ans[arr[i]] = s
-                s = 1
-        ans[arr[n-1]] = s
-        if len(set(ans.values())) == len(ans):
-            return True
-        else:
-            return False
-
-
-        
-        
+        # n = len(arr)
+        # s = 1
+        # arr.sort()
+        # print(arr)
+        # ans = {}
+        # if n == 1:
+        #     return True
+        # for i in range(n-1):
+        #     if arr[i+1] == arr[i]:
+        #         s += 1
+        #     else:
+        #         ans[arr[i]] = s
+        #         s = 1
+        # ans[arr[n-1]] = s
+        # if len(set(ans.values())) == len(ans):
+        #     return True
+        # else:
+        #     return False
+        c = Counter(arr).values()
+        return len(c)==len(set(c))
