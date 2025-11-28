eab8b946cd744e96124a3dd0b3e740de662c9401
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2025-11-28 17:44:09 -0500

    Time: 75 ms (27.03%), Space: 18.1 MB (54.88%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -6,8 +6,8 @@ class Solution:
                 d[char] += 1
             else:
                 d[char] = 1
-        for i, char in enumerate(s):
-            if d[char] == 1:
+        for i in range(len(s)):
+            if d[s[i]] == 1:
                 return i
         return -1
 
