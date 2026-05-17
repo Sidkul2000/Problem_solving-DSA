f6b4538cd740851782cd7681cc90194283d9c0f1
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-05-17 12:24:49 -0400

    Time: 241 ms (6.43%), Space: 20.7 MB (43.43%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,18 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        def feasible(speed):
-            s = sum((p-1)//speed+1 for p in piles)
+        l = 1
+        r = max(piles)
+        while l<=r:
+            s=0
+            mid = (l+r)//2
+            for i in piles:
+                s += int((i-1)/mid + 1)
             if s <= h:
-                return True
-            else:
-                return False
-
-        n = len(piles)
-        l, r = 1, max(piles)
-        while l<r:
-            m = (l+r)//2
-            if feasible(m):
-                r = m
-            else:
-                l = m+1
+                r = mid-1
+            elif s > h:
+                l = mid+1
+    
         return l
+        
