b6346afdfd02332b7bceec821fadc4cde2984652
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-05-28 19:12:59 -0400

    Time: 4 ms (14.29%), Space: 19.2 MB (87.73%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,24 +1,23 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        if not chars:
-            return 0
         n = len(chars)
-        s = ""
-        cnt = 1
-        temp = chars[0]
-        for i in range(1, n):
-            if chars[i] != temp:
-                temp = chars[i]
-                s = s + chars[i-1]
-                if cnt != 1:
-                    s = s + str(cnt)
-                cnt = 1
+        if n==1:
+            return n
+        c = chars[0]
+        s = 1
+        for i in range(1,n):
+            if chars[i] == chars[i-1]:
+                s+=1
             else:
-                cnt += 1
-        s += chars[n-1]
-        if cnt != 1:
-            s += str(cnt)
-        for i, char in enumerate(s):
-            chars.insert(i, char)
-        return len(s)
+                if s>1:
+                    c += str(s)
+                    s=1
+                c += chars[i]
+        if s>1:
+            c += str(s)
+        for i in range(len(c)):
+            chars[i] = c[i]
+        return len(c)
+
+
 
