cfa66df755c26e7b64eff387c3edad5425cfcf1d
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-05-29 00:24:38 -0400

    Time: 35 ms (10.31%), Space: 19.6 MB (84.23%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -2,33 +2,34 @@ from collections import deque
 
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
         m = len(grid)
         n = len(grid[0])
-        maxArea = 0
-        visit = set()
+        seen = set()
+        maxa = 0
 
         def bfs(r,c):
-            queue = deque()
-            queue.append((r,c))
-            visit.add((r,c))
-            area = 1
-            while queue:
-                row, col = queue.popleft()
-                directions = [[0,1],[1,0],[0,-1],[-1,0]]
-                for dr, dc in directions:
-                    ro = row + dr
-                    co = col + dc
-                    if (ro in range(m) and co in range(n) and grid[ro][co]==1 and (ro,co) not in visit):
-                        area += 1
-                        visit.add((ro,co))
-                        queue.append((ro,co))
-            return area
+            seen.add((r,c))
 
-        for r in range(m):
-            for c in range(n):
-                if grid[r][c]==1 and (r,c) not in visit:
-                    a = bfs(r,c)
-                    maxArea = max(maxArea, a)
-        return maxArea
+            q = deque()
+            q.append((r,c))
+            area = 1
+            while q:
+                row, col = q.popleft()
+                directions = [[0,1],[1,0],[-1,0],[0,-1]]
+                for dr, dc in directions:
+                    ro = row+dr
+                    co = col+dc
+                    if ro in range(m) and co in range(n) and grid[ro][co]==1 and (ro,co) not in seen:
+                        area += 1
+                        seen.add((ro,co))
+                        q.append((ro,co))
+            return area
+                
+
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1 and (i,j) not in seen:
+                    area = bfs(i,j)
+                    maxa = max(maxa, area)
+        return maxa
