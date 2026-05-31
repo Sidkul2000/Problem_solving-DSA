b2f001e93d9ec7245c725a78306bd91225f1168b
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-05-30 21:40:28 -0400

    Time: 0 ms (100%), Space: 19.5 MB (69.06%) - LeetHub

--- 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
+++ 0166-fraction-to-recurring-decimal/0166-fraction-to-recurring-decimal.py
@@ -1,33 +1,30 @@
 class Solution:
     def fractionToDecimal(self, numerator: int, denominator: int) -> str:
-        if numerator % denominator == 0:
+        if (numerator % denominator)==0:
             return str(numerator//denominator)
-        ans = []
         negative = (numerator < 0) ^ (denominator < 0)
-        if negative:
-            ans.append("-")
-        n = abs(numerator)
-        d = abs(denominator)
+        num, den = abs(numerator), abs(denominator)
 
-        ans.append(str(n//d))
-        ans.append(".")
+        integer = num//den
+        result = "-" if negative else ""
+        result += str(integer) + "."
 
-        rem = n % d
-        seen = {}
-        idx = 0
+        rem = num%den
+        mp = {}
+        decimal = []
 
         while rem != 0:
-            if rem in seen:
-                idx = seen[rem]
-                ans.insert(idx, "(")
-                ans.append(")")
-                return "".join(ans)
-            seen[rem] = len(ans)
+            if rem in mp:
+                repeat = mp[rem]
+                decimal.insert(repeat, "(")
+                decimal.append(")")
+                break
+            
+            mp[rem] = len(decimal)
             rem *= 10
-            ans.append(str(rem // d))
-            rem %= d
-        return "".join(ans)
-
-
-
+            digit = rem//den
+            decimal.append(str(digit))
+            rem %= den
         
+        return result + "".join(decimal)
+            
