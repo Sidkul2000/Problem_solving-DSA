7b78ed94af77dd989c6f84c116dd199e7598f25c
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-06-29 18:49:54 -0400

    Time: 183 ms (68.77%), Space: 26 MB (13.33%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -5,13 +5,10 @@ class Solution:
         res = 0
         for r in range(len(fruits)):
             mp[fruits[r]] += 1
-            while len(mp)>2:
+            while len(mp) > 2:
                 mp[fruits[l]] -= 1
                 if mp[fruits[l]] == 0:
                     del mp[fruits[l]]
                 l += 1
             res = max(res, r-l+1)
         return res
-
-            
-        
