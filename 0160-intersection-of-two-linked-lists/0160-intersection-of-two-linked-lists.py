dd10cfbedc96743c68651b153fe9810a6a461c87
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-07-05 09:46:32 -0400

    Time: 118 ms (30.36%), Space: 38.5 MB (7.89%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.py
@@ -6,15 +6,29 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        # a = headA
+        # b = headB
+        # while a!=b:
+        #     if a:
+        #         a = a.next
+        #     else:
+        #         a = headB
+        #     if b:
+        #         b = b.next
+        #     else:
+        #         b = headA
+        # return a
+
+        seen = set()
         a = headA
         b = headB
-        while a!=b:
-            if a:
-                a = a.next
-            else:
-                a = headB
-            if b:
-                b = b.next
-            else:
-                b = headA
-        return a
+
+        while a:
+            seen.add(a)
+            a = a.next
+        
+        while b:
+            if b in seen:
+                return b
+            b = b.next
+        return None
