4d538cd47a2982969cff176b2f19992a23abf033
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-07-08 18:42:46 -0400

    Time: 315 ms (12.31%), Space: 23.8 MB (65.5%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -7,12 +7,12 @@ class Solution:
             d = 1
             w = 0
             for i in range(n):
-                if weights[i] + w > m:
-                    d += 1
+                if w+weights[i] > m:
                     w = 0
+                    d += 1
                 w += weights[i]
             if d > days:
                 l = m + 1
             else:
                 r = m
-        return l
+        return r 
