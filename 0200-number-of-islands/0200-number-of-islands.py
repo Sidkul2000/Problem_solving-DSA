b7d6371ba0bb610fc65cb72574db92b6812321b4
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-07-15 22:57:18 -0400

    Time: 299 ms (5.98%), Space: 26.9 MB (17.05%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -2,31 +2,28 @@ from collections import deque
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
-        seen = set()
         m = len(grid)
         n = len(grid[0])
+        seen = set()
+        islands = 0
+        d = [[1,0], [0,1], [-1,0], [0,-1]]
 
-        def bfs(r,c):
-            q = deque()
-            q.append((r,c))
+        def dfs(r,c):
             seen.add((r,c))
-            while q:
-                row, col = q.popleft()
-                directions = [[1,0],[-1,0],[0,1],[0,-1]]
-                for dr, dc in directions:
-                    ro = row+dr
-                    co = col+dc
-                    if ro in range(m) and co in range(n) and grid[ro][co]=="1" and (ro,co) not in seen:
-                        seen.add((ro,co))
-                        q.append((ro,co))
-
+            queue = deque()
+            queue.append((r,c))
+            while queue:
+                ro, co = queue.popleft()
+                for dr, dc in d:
+                    row = ro + dr
+                    col = co + dc
+                    if row in range(m) and col in range(n) and grid[row][col]=="1" and (row,col) not in seen:
+                        seen.add((row,col))
+                        queue.append((row,col))
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]=="1" and (i,j) not in seen:
+                if grid[i][j] == "1" and (i,j) not in seen:
                     islands += 1
-                    bfs(i,j)
-    
+                    dfs(i,j)
         return islands
-        
