f75cbc6e68a0a01c6059a073e9ecd4149873f05d
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-07-16 20:24:24 -0400

    Time: 4 ms (37.47%), Space: 19.3 MB (78.53%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -1,33 +1,30 @@
 from collections import deque
 
 class Solution:
-    def orangesRotting(self, grid):
-        m, n = len(grid), len(grid[0])
-        q = deque()
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
         fresh = 0
+        d = [[0,1],[0,-1],[1,0],[-1,0]]
+        minutes = 0
+        queue = deque()
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == 2:
-                    q.append((i, j))
-                elif grid[i][j] == 1:
+                if grid[i][j]==2:
+                    queue.append((i,j))
+                elif grid[i][j]==1:
                     fresh += 1
 
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        minutes = 0
-
-        while q and fresh > 0:
-            for _ in range(len(q)):
-                x, y = q.popleft()
-
-                for dx, dy in directions:
-                    nx, ny = x + dx, y + dy
-
-                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
-                        grid[nx][ny] = 2
+        while queue and fresh > 0:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                for dr, dc in d:
+                    ro = r+dr
+                    co = c+dc
+                    if ro in range(m) and co in range(n) and grid[ro][co]==1:
+                        grid[ro][co] = 2
                         fresh -= 1
-                        q.append((nx, ny))
-
+                        queue.append((ro,co))
             minutes += 1
-
-        return minutes if fresh == 0 else -1
+        return minutes if fresh==0 else -1
