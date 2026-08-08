fb801e212581a555be6dc871e1f56544bac20f04
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-08-07 23:48:17 -0400

    Time: 7 ms (16.04%), Space: 19.3 MB (41.27%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -5,26 +5,29 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         fresh = 0
-        d = [[0,1],[0,-1],[1,0],[-1,0]]
-        minutes = 0
         queue = deque()
+        d = [[1,0],[0,1],[-1,0],[0,-1]]
+        minutes = 0
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j]==2:
+                if grid[i][j] == 2:
                     queue.append((i,j))
-                elif grid[i][j]==1:
+                elif grid[i][j] == 1:
                     fresh += 1
 
         while queue and fresh > 0:
-            for i in range(len(queue)):
-                r, c = queue.popleft()
-                for dr, dc in d:
-                    ro = r+dr
-                    co = c+dc
-                    if ro in range(m) and co in range(n) and grid[ro][co]==1:
-                        grid[ro][co] = 2
-                        fresh -= 1
-                        queue.append((ro,co))
             minutes += 1
+            for l in range(len(queue)):
+                row, col = queue.popleft()
+                for dr, dc in d:
+                    r = row+dr
+                    c = col+dc
+                    if r in range(m) and c in range(n) and grid[r][c]==1:
+                        grid[r][c] = 2
+                        fresh -= 1
+                        queue.append((r,c))
+        
         return minutes if fresh==0 else -1
+        
+
