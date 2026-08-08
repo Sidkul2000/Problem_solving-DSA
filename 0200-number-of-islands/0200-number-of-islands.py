d10ea0e84c6fd57c1c11375adc52a4f92dc3b6d0
Siddhant Kulkarni <63728680+Sidkul2000@users.noreply.github.com>
2026-08-07 20:41:43 -0400

    Time: 307 ms (5.21%), Space: 31.4 MB (6.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -6,24 +6,32 @@ class Solution:
         n = len(grid[0])
         seen = set()
         islands = 0
-        d = [[1,0], [0,1], [-1,0], [0,-1]]
+        d = [[0,1],[0,-1],[1,0],[-1,0]]
 
         def dfs(r,c):
+            # queue = deque()
+            # queue.append((r,c))
+            # seen.add((i,j))
+            # while queue:
+            #     ro, co = queue.popleft()
+            #     for dr, dc in d:
+            #         row = ro+dr
+            #         col = co+dc
+            #         if row in range(m) and col in range(n) and (row,col) not in seen and grid[row][col]=="1":
+            #             seen.add((row,col))
+            #             queue.append((row,col))
             seen.add((r,c))
-            queue = deque()
-            queue.append((r,c))
-            while queue:
-                ro, co = queue.popleft()
-                for dr, dc in d:
-                    row = ro + dr
-                    col = co + dc
-                    if row in range(m) and col in range(n) and grid[row][col]=="1" and (row,col) not in seen:
-                        seen.add((row,col))
-                        queue.append((row,col))
+            for dr, dc in d:
+                ro, co = r+dr, c+dc
+                if ro in range(m) and co in range(n) and (ro,co) not in seen and grid[ro][co]=="1":
+                    dfs(ro,co)
+
+
+
 
         for i in range(m):
             for j in range(n):
-                if grid[i][j] == "1" and (i,j) not in seen:
+                if grid[i][j]=="1" and (i,j) not in seen:
                     islands += 1
                     dfs(i,j)
         return islands
